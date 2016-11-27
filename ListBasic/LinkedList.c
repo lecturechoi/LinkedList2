d9c330526450363bfc6b591c2724bdc4cac4abee
Jungsik Choi <lecture.choi@gmail.com>
2016-11-27 21:46:54 +0900

    기본 소스코드

---
+++ ListBasic/LinkedList.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+void init_list(List *list)
+{
+	// initialize the list
+}
+
+void add_to_list(List *list, int data)
+{
+	// add the data to the list
+}
+
+void print_list(List *list)
+{
+	// print all the data of the list
+}
+
+void delete_list(List *list)
+{
+	// delete the list
+}
